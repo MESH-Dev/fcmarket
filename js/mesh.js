ae17de34e7686017d9b743ea4216aadc3f8dd347
karlshaver <karl@meshfresh.com>
2017-11-27 17:22:33 -0500

    completed styling, functional mobile menu and desktop dropdowns

--- js/mesh.js
+++ js/mesh.js
@@ -1,6 +1,16 @@
 jQuery(document).ready(function($){
 
+   $('.main-navigation, #menu-trigger').addClass('js');
+
    $('.parallax').parallax("50%", 0.5);
+
+   $('#menu-trigger').click(function(){
+      $('.main-navigation').slideToggle().addClass('open');
+   });
+
+   $('li.menu-item-has-children').click(function(){
+      $(this).children('.sub-menu').slideToggle();
+   });
 
   //Let's do something awesome!
 
